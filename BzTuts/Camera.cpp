14dddc49e52cd2ffba14d57d6cf5d1a39187df5d
hunagqi <452560911@qq.com>
2019-02-28 14:35:58 +0800

    delete old camera

--- BzTuts/Camera.cpp
+++ BzTuts/Camera.cpp
@@ -2,272 +2,130 @@
 
 #include "Camera.h"
 
-using namespace DirectX;
 
-Camera::Camera()
+
+
+void NCamera::SetPerspectiveMatrix(float FovAngleY, float AspectRatio, float NearZ, float FarZ)
 {
-	SetLens(0.25f*3.1415926, 1.0f, 1.0f, 1000.0f);
+	float    SinFov;
+	float    CosFov;
+	XMScalarSinCos(&SinFov, &CosFov, 0.5f * FovAngleY);
+
+	float Height = CosFov / SinFov;
+	float Width = Height / AspectRatio;
+	float fRange = FarZ / (FarZ - NearZ);
+
+	mProj.M[0][0] = Width;
+	mProj.M[0][1] = 0.0f;
+	mProj.M[0][2] = 0.0f;
+	mProj.M[0][3] = 0.0f;
+
+	mProj.M[1][0] = 0.0f;
+	mProj.M[1][1] = Height;
+	mProj.M[1][2] = 0.0f;
+	mProj.M[1][3] = 0.0f;
+
+	mProj.M[2][0] = 0.0f;
+	mProj.M[2][1] = 0.0f;
+	mProj.M[2][2] = fRange;
+	mProj.M[2][3] = 1.0f;
+
+	mProj.M[3][0] = 0.0f;
+	mProj.M[3][1] = 0.0f;
+	mProj.M[3][2] = -fRange * NearZ;
+	mProj.M[3][3] = 0.0f;
 }
 
-Camera::~Camera()
+void NCamera::SetLens(float fovY, float aspect, float zn, float zf)
 {
-}
-
-XMVECTOR Camera::GetPosition()const
-{
-	return XMLoadFloat3(&mPosition);
-}
-
-XMFLOAT3 Camera::GetPosition3f()const
-{
-	return mPosition;
-}
-
-void Camera::SetPosition(float x, float y, float z)
-{
-	mPosition = XMFLOAT3(x, y, z);
-	mViewDirty = true;
-}
-
-void Camera::SetPosition(const XMFLOAT3& v)
-{
-	mPosition = v;
-	mViewDirty = true;
-}
-
-XMVECTOR Camera::GetRight()const
-{
-	return XMLoadFloat3(&mRight);
-}
-
-XMFLOAT3 Camera::GetRight3f()const
-{
-	return mRight;
-}
-
-XMVECTOR Camera::GetUp()const
-{
-	return XMLoadFloat3(&mUp);
-}
-
-XMFLOAT3 Camera::GetUp3f()const
-{
-	return mUp;
-}
-
-XMVECTOR Camera::GetLook()const
-{
-	return XMLoadFloat3(&mLook);
-}
-
-XMFLOAT3 Camera::GetLook3f()const
-{
-	return mLook;
-}
-
-float Camera::GetNearZ()const
-{
-	return mNearZ;
-}
-
-float Camera::GetFarZ()const
-{
-	return mFarZ;
-}
-
-float Camera::GetAspect()const
-{
-	return mAspect;
-}
-
-float Camera::GetFovY()const
-{
-	return mFovY;
-}
-
-float Camera::GetFovX()const
-{
-	float halfWidth = 0.5f*GetNearWindowWidth();
-	return 2.0f*atan(halfWidth / mNearZ);
-}
-
-float Camera::GetNearWindowWidth()const
-{
-	return mAspect * mNearWindowHeight;
-}
-
-float Camera::GetNearWindowHeight()const
-{
-	return mNearWindowHeight;
-}
-
-float Camera::GetFarWindowWidth()const
-{
-	return mAspect * mFarWindowHeight;
-}
-
-float Camera::GetFarWindowHeight()const
-{
-	return mFarWindowHeight;
-}
-
-void Camera::SetLens(float fovY, float aspect, float zn, float zf)
-{
-	// cache properties
 	mFovY = fovY;
 	mAspect = aspect;
 	mNearZ = zn;
 	mFarZ = zf;
-
 	mNearWindowHeight = 2.0f * mNearZ * tanf(0.5f*mFovY);
 	mFarWindowHeight = 2.0f * mFarZ * tanf(0.5f*mFovY);
 
-	XMMATRIX P = XMMatrixPerspectiveFovLH(mFovY, mAspect, mNearZ, mFarZ);
-	XMStoreFloat4x4(&mProj, P);
+	SetPerspectiveMatrix(mFovY, mAspect, mNearZ, mFarZ);
 }
 
-void Camera::LookAt(FXMVECTOR pos, FXMVECTOR target, FXMVECTOR worldUp)
+void NCamera::LookAt(const Vector& pos, const Vector& target, const Vector& up)
 {
-	XMVECTOR L = XMVector3Normalize(XMVectorSubtract(target, pos));
-	XMVECTOR R = XMVector3Normalize(XMVector3Cross(worldUp, L));
-	XMVECTOR U = XMVector3Cross(L, R);
+	mLook = (target - pos).GetSafeNormal();
+	mRight = (up^mLook).GetSafeNormal();
+	mUp = mLook ^ mRight;
+	mPosition = pos;
+	mViewDirty = true;
+}
 
-	XMStoreFloat3(&mPosition, pos);
-	XMStoreFloat3(&mLook, L);
-	XMStoreFloat3(&mRight, R);
-	XMStoreFloat3(&mUp, U);
+void NCamera::Strafe(float d)
+{
+	mPosition = d * mRight + mPosition;
+	mViewDirty = true;
+}
+
+void NCamera::Walk(float d)
+{
+
+	mPosition = d * mLook + mPosition;
 
 	mViewDirty = true;
 }
 
-void Camera::LookAt(const XMFLOAT3& pos, const XMFLOAT3& target, const XMFLOAT3& up)
+void NCamera::Pitch(float angle)
 {
-	XMVECTOR P = XMLoadFloat3(&pos);
-	XMVECTOR T = XMLoadFloat3(&target);
-	XMVECTOR U = XMLoadFloat3(&up);
+	RotationMatrix R(Rotator(0, 0, angle));
+	mUp = R.TransformPosition(mUp);
+	mLook = R.TransformPosition(mLook);
+	mViewDirty = true;
+}
 
-	LookAt(P, T, U);
+void NCamera::RotateY(float angle)
+{
+	RotationMatrix Res(Rotator(angle, 0, 0));
+
+	mRight = Res.TransformPosition(mRight);
+	mUp = Res.TransformPosition(mUp);
+	mLook = Res.TransformPosition(mLook);
 
 	mViewDirty = true;
 }
 
-XMMATRIX Camera::GetView()const
-{
-	assert(!mViewDirty);
-	return XMLoadFloat4x4(&mView);
-}
-
-XMMATRIX Camera::GetProj()const
-{
-	return XMLoadFloat4x4(&mProj);
-}
-
-
-XMFLOAT4X4 Camera::GetView4x4f()const
-{
-	assert(!mViewDirty);
-	return mView;
-}
-
-XMFLOAT4X4 Camera::GetProj4x4f()const
-{
-	return mProj;
-}
-
-void Camera::Strafe(float d)
-{
-	// mPosition += d*mRight
-	XMVECTOR s = XMVectorReplicate(d);
-	XMVECTOR r = XMLoadFloat3(&mRight);
-	XMVECTOR p = XMLoadFloat3(&mPosition);
-	XMStoreFloat3(&mPosition, XMVectorMultiplyAdd(s, r, p));
-
-	mViewDirty = true;
-}
-
-void Camera::Walk(float d)
-{
-	// mPosition += d*mLook
-	XMVECTOR s = XMVectorReplicate(d);
-	XMVECTOR l = XMLoadFloat3(&mLook);
-	XMVECTOR p = XMLoadFloat3(&mPosition);
-	XMStoreFloat3(&mPosition, XMVectorMultiplyAdd(s, l, p));
-
-	mViewDirty = true;
-}
-
-void Camera::Pitch(float angle)
-{
-	// Rotate up and look vector about the right vector.
-
-	XMMATRIX R = XMMatrixRotationAxis(XMLoadFloat3(&mRight), angle);
-
-	XMStoreFloat3(&mUp, XMVector3TransformNormal(XMLoadFloat3(&mUp), R));
-	XMStoreFloat3(&mLook, XMVector3TransformNormal(XMLoadFloat3(&mLook), R));
-
-	mViewDirty = true;
-}
-
-void Camera::RotateY(float angle)
-{
-	// Rotate the basis vectors about the world y-axis.
-
-	XMMATRIX R = XMMatrixRotationY(angle);
-
-	XMStoreFloat3(&mRight, XMVector3TransformNormal(XMLoadFloat3(&mRight), R));
-	XMStoreFloat3(&mUp, XMVector3TransformNormal(XMLoadFloat3(&mUp), R));
-	XMStoreFloat3(&mLook, XMVector3TransformNormal(XMLoadFloat3(&mLook), R));
-
-	mViewDirty = true;
-}
-
-void Camera::UpdateViewMatrix()
+void NCamera::UpdateViewMatrix()
 {
 	if (mViewDirty)
 	{
-		XMVECTOR R = XMLoadFloat3(&mRight);
-		XMVECTOR U = XMLoadFloat3(&mUp);
-		XMVECTOR L = XMLoadFloat3(&mLook);
-		XMVECTOR P = XMLoadFloat3(&mPosition);
 
-		// Keep camera's axes orthogonal to each other and of unit length.
-		L = XMVector3Normalize(L);
-		U = XMVector3Normalize(XMVector3Cross(L, R));
+		mLook.Normalize();
+		mUp = (mLook ^ mRight).GetSafeNormal();
+		mRight = mUp ^ mLook;
+		mRight.Normalize();
 
-		// U, L already ortho-normal, so no need to normalize cross product.
-		R = XMVector3Cross(U, L);
+		float x = -((mPosition | mRight));
+		float y = -((mPosition | mUp));
+		float z = -((mPosition | mLook));
 
-		// Fill in the view matrix entries.
-		float x = -XMVectorGetX(XMVector3Dot(P, R));
-		float y = -XMVectorGetX(XMVector3Dot(P, U));
-		float z = -XMVectorGetX(XMVector3Dot(P, L));
 
-		XMStoreFloat3(&mRight, R);
-		XMStoreFloat3(&mUp, U);
-		XMStoreFloat3(&mLook, L);
 
-		mView(0, 0) = mRight.x;
-		mView(1, 0) = mRight.y;
-		mView(2, 0) = mRight.z;
-		mView(3, 0) = x;
+		mView.M[0][0] = mRight.X;
+		mView.M[1][0] = mRight.Y;
+		mView.M[2][0] = mRight.Z;
+		mView.M[3][0] = x;
 
-		mView(0, 1) = mUp.x;
-		mView(1, 1) = mUp.y;
-		mView(2, 1) = mUp.z;
-		mView(3, 1) = y;
+		mView.M[0][1] = mUp.X;
+		mView.M[1][1] = mUp.Y;
+		mView.M[2][1] = mUp.Z;
+		mView.M[3][1] = y;
 
-		mView(0, 2) = mLook.x;
-		mView(1, 2) = mLook.y;
-		mView(2, 2) = mLook.z;
-		mView(3, 2) = z;
+		mView.M[0][2] = mLook.X;
+		mView.M[1][2] = mLook.Y;
+		mView.M[2][2] = mLook.Z;
+		mView.M[3][2] = z;
 
-		mView(0, 3) = 0.0f;
-		mView(1, 3) = 0.0f;
-		mView(2, 3) = 0.0f;
-		mView(3, 3) = 1.0f;
-
+		mView.M[0][3] = 0.0f;
+		mView.M[1][3] = 0.0f;
+		mView.M[2][3] = 0.0f;
+		mView.M[3][3] = 1.0f;
+		//mView=mView.GetTransposed();
 		mViewDirty = false;
 	}
 }
-
-
--- BzTuts/Camera.h
+++ BzTuts/Camera.h
@@ -1,98 +1,4 @@
 ﻿#include "d3dx12.h"
-#include <DirectXMath.h>
-class Camera
-{
-public:
-
-	Camera();
-	~Camera();
-
-	// Get/Set world camera position.
-	DirectX::XMVECTOR GetPosition()const;
-	DirectX::XMFLOAT3 GetPosition3f()const;
-	void SetPosition(float x, float y, float z);
-	void SetPosition(const DirectX::XMFLOAT3& v);
-
-	// Get camera basis vectors.
-	DirectX::XMVECTOR GetRight()const;
-	DirectX::XMFLOAT3 GetRight3f()const;
-	DirectX::XMVECTOR GetUp()const;
-	DirectX::XMFLOAT3 GetUp3f()const;
-	DirectX::XMVECTOR GetLook()const;
-	DirectX::XMFLOAT3 GetLook3f()const;
-
-	// Get frustum properties.
-	float GetNearZ()const;
-	float GetFarZ()const;
-	float GetAspect()const;
-	float GetFovY()const;
-	float GetFovX()const;
-
-	// Get near and far plane dimensions in view space coordinates.
-	float GetNearWindowWidth()const;
-	float GetNearWindowHeight()const;
-	float GetFarWindowWidth()const;
-	float GetFarWindowHeight()const;
-
-	// Set frustum.
-	void SetLens(float fovY, float aspect, float zn, float zf);
-
-	// Define camera space via LookAt parameters.
-	void LookAt(DirectX::FXMVECTOR pos, DirectX::FXMVECTOR target, DirectX::FXMVECTOR worldUp);
-	void LookAt(const DirectX::XMFLOAT3& pos, const DirectX::XMFLOAT3& target, const DirectX::XMFLOAT3& up);
-
-	// Get View/Proj matrices.
-	DirectX::XMMATRIX GetView()const;
-	DirectX::XMMATRIX GetProj()const;
-
-	DirectX::XMFLOAT4X4 GetView4x4f()const;
-	DirectX::XMFLOAT4X4 GetProj4x4f()const;
-
-	// Strafe/Walk the camera a distance d.
-	void Strafe(float d);
-	void Walk(float d);
-
-	// Rotate the camera.
-	void Pitch(float angle);
-	void RotateY(float angle);
-
-	// After modifying camera position/orientation, call to rebuild the view matrix.
-	void UpdateViewMatrix();
-
-private:
-
-	// Camera coordinate system with coordinates relative to world space.
-	DirectX::XMFLOAT3 mPosition = { 0.0f, 0.0f, 0.0f };
-	DirectX::XMFLOAT3 mRight = { 1.0f, 0.0f, 0.0f };
-	DirectX::XMFLOAT3 mUp = { 0.0f, 1.0f, 0.0f };
-	DirectX::XMFLOAT3 mLook = { 0.0f, 0.0f, 1.0f };
-
-	// Cache frustum properties.
-	float mNearZ = 0.0f;
-	float mFarZ = 0.0f;
-	float mAspect = 0.0f;
-	float mFovY = 0.0f;
-	float mNearWindowHeight = 0.0f;
-	float mFarWindowHeight = 0.0f;
-
-	bool mViewDirty = true;
-
-	// Cache View/Proj matrices.
-	DirectX::XMFLOAT4X4 mView = DirectX::XMFLOAT4X4(
-		1.0f, 0.0f, 0.0f, 0.0f,
-		0.0f, 1.0f, 0.0f, 0.0f,
-		0.0f, 0.0f, 1.0f, 0.0f,
-		0.0f, 0.0f, 0.0f, 1.0f);
-	DirectX::XMFLOAT4X4 mProj = DirectX::XMFLOAT4X4(
-			1.0f, 0.0f, 0.0f, 0.0f,
-			0.0f, 1.0f, 0.0f, 0.0f,
-			0.0f, 0.0f, 1.0f, 0.0f,
-			0.0f, 0.0f, 0.0f, 1.0f);
-};
-
-
-
-
 #include"CameraType.h"
 #include"OCMath.h"
 using namespace oocd;
@@ -130,144 +36,25 @@ public:
 	void SetPerspectiveMatrix(float FovAngleY,
 		float AspectRatio,
 		float NearZ,
-		float FarZ)
-	{
+		float FarZ);
+	void SetLens(float fovY, float aspect, float zn, float zf);;
 
-		float    SinFov;
-		float    CosFov;
-		XMScalarSinCos(&SinFov, &CosFov, 0.5f * FovAngleY);
+	void LookAt(const Vector& pos, const Vector& target, const Vector& up);
 
-		float Height = CosFov / SinFov;
-		float Width = Height / AspectRatio;
-		float fRange = FarZ / (FarZ - NearZ);
-
-		mProj.M[0][0] = Width;
-		mProj.M[0][1] = 0.0f;
-		mProj.M[0][2] = 0.0f;
-		mProj.M[0][3] = 0.0f;
-
-		mProj.M[1][0] = 0.0f;
-		mProj.M[1][1] = Height;
-		mProj.M[1][2] = 0.0f;
-		mProj.M[1][3] = 0.0f;
-
-		mProj.M[2][0] = 0.0f;
-		mProj.M[2][1] = 0.0f;
-		mProj.M[2][2] = fRange;
-		mProj.M[2][3] = 1.0f;
-
-		mProj.M[3][0] = 0.0f;
-		mProj.M[3][1] = 0.0f;
-		mProj.M[3][2] = -fRange * NearZ;
-		mProj.M[3][3] = 0.0f;
-
-	}
-	void SetLens(float fovY, float aspect, float zn, float zf)
-
-	{
-		mFovY = fovY;
-		mAspect = aspect;
-		mNearZ = zn;
-		mFarZ = zf;
-		mNearWindowHeight = 2.0f * mNearZ * tanf(0.5f*mFovY);
-		mFarWindowHeight = 2.0f * mFarZ * tanf(0.5f*mFovY);
-
-		SetPerspectiveMatrix(mFovY, mAspect, mNearZ, mFarZ);
-	
-	};
-
-	void LookAt(const Vector& pos, const Vector& target, const Vector& up)
-	{
-		mLook  = (target-pos).GetSafeNormal();
-		mRight = (up^mLook).GetSafeNormal();
-		mUp = mLook ^ mRight;
-		mPosition=pos;
-		mViewDirty = true;
-	}
-
-	// Get View/Proj matrices.
 	Matrix GetView()const { return mView; };
 	Matrix GetProj()const { return mProj; }
 	Matrix GetView4x4f()const { return mView; };
 
-	// Strafe/Walk the camera a distance d.
-	void Strafe(float d)
-	{
-
-		mPosition = d * mRight + mPosition;
-		mViewDirty = true;
-	}
-	void Walk(float d)
-	{
-	
-		mPosition = d * mLook + mPosition;
-
-		mViewDirty = true;
-	}
+	void Strafe(float d);
+	void Walk(float d);
 
 	// Rotate the camera.
-	void Pitch(float angle)
-	{
+	void Pitch(float angle);
+	void RotateY(float angle);
 
-		RotationMatrix R(Rotator(0,0, angle));
-		mUp = R.TransformPosition(mUp);
-		mLook = R.TransformPosition(mLook);
-		mViewDirty = true;
-	}
-	void RotateY(float angle)
-	{
-		RotationMatrix Res(Rotator(angle, 0, 0));
-
-		mRight = Res.TransformPosition(mRight);
-		mUp = Res.TransformPosition(mUp);
-		mLook = Res.TransformPosition(mLook);
-
-		mViewDirty = true;
-	}
-
-	// After modifying camera position/orientation, call to rebuild the view matrix.
-	void UpdateViewMatrix()
-	{
-		if (mViewDirty)
-		{
-		
-			mLook.Normalize();
-			mUp = (mLook ^ mRight).GetSafeNormal();
-			mRight = mUp ^ mLook;
-			mRight.Normalize();
-
-			float x = -((mPosition | mRight));
-			float y = -((mPosition | mUp));
-			float z = -((mPosition | mLook));
-
-		
-
-			mView.M[0][ 0] = mRight.X;
-			mView.M[1][ 0] = mRight.Y;
-			mView.M[2][ 0] = mRight.Z;
-			mView.M[3][ 0] = x;
-
-			mView.M[0][ 1] = mUp.X;
-			mView.M[1][ 1] = mUp.Y;
-			mView.M[2][ 1] = mUp.Z;
-			mView.M[3][ 1] = y;
-
-			mView.M[0][ 2] = mLook.X;
-			mView.M[1][ 2] = mLook.Y;
-			mView.M[2][ 2] = mLook.Z;
-			mView.M[3][ 2] = z;
-
-			mView.M[0][ 3] = 0.0f;
-			mView.M[1][ 3] = 0.0f;
-			mView.M[2][ 3] = 0.0f;
-			mView.M[3][ 3] = 1.0f;
-			//mView=mView.GetTransposed();
-			mViewDirty = false;
-		}
-	}
+	void UpdateViewMatrix();
 private:
 
-	// Camera coordinate system with coordinates relative to world space.
 	Vector mPosition = { 0.0f, 0.0f, 0.0f };
 	Vector mRight = { 1.0f, 0.0f, 0.0f };
 	Vector mUp = { 0.0f, 1.0f, 0.0f };
